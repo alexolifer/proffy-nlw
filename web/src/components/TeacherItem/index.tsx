6811131d8e0f7b3db5c6e7c5b5a24a61015fd962
Monica Hillman <51282495+MonicaHillman@users.noreply.github.com>
2020-08-05 13:45:07 -0300

    Developed TeacherForm and connect to db

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,36 +2,60 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
-return (
-<article className="teacher-item">
-    <header>
-        <img src="https://avatars0.githubusercontent.com/u/51282495?s=460&u=82be8af0981a38fa9e6cf2e514b73227326d547e&v=4" alt="Monica Hillman" />
-        <div>
-            <strong>Monica Hillman</strong>
-            <span>Matemática</span>
-        </div>
-    </header>
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+};
 
-    <p>
-        A Matemática é uma ciência que relaciona a lógica com situações práticas habituais.
-<br /><br />
-Ela desenvolve uma constante busca pela veracidade dos fatos por meio de técnicas precisas e exatas. Ao longo da história, a Matemática foi sendo construída e aperfeiçoada, prosseguindo em constante evolução, investigando novas situações e estabelecendo relações com os acontecimentos cotidianos.
-</p>
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
 
-    <footer>
-        <p>
-            Preço/hora
-        <strong>R$ 80,00</strong>
-        </p>
-        <button type="button">
-            <img src={whatsappIcon} alt="Whatsapp" />
-        Entrar em contato
-    </button>
-    </footer>
-</article>
-);
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+    
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src={teacher.avatar} alt={teacher.name} />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+
+            <p> {teacher.bio}</p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a
+                // eslint-disable-next-line
+                target="_blank" 
+               
+                onClick={createNewConnection} 
+                href={`https://wa.me/${teacher.whatsapp}`}>
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                    Entrar em contato
+                </a>
+            </footer>
+        </article>
+    );
 }
 
 export default TeacherItem;
